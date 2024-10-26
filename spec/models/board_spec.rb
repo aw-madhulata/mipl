1ed5b7931f82e6c6e816101a55eb5e3d1d6f458e
Awijeet Shandilya <aw@madhulata.org>
2024-10-26 10:53:22 +0530

    rspec added for board new

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Board, type: :model do
   it { should belong_to(:user) }
-  it { is_expected.to validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
   # pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Boards", type: :request do
-  describe "GET /index" do
+  let(:user) { create(:user) }
+  before do
+    sign_in user  # Sign in the user
+  end
+  
+  describe "GET boards/index" do
     it "succeeds" do
       get new_board_path
       expect(response).to have_http_status(200)
